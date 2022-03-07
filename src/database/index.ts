62e9fab78c27c632743e04586f3ee34fcc7ad2d3
luisaraujodev <luisjtwo@gmail.com>
2022-03-07 19:28:09 -0300

    create connection to database

---
+++ src/database/index.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import { createConnection, ConnectionOptions } from "typeorm";
+import config from "../../ormconfig";
+
+export const createConnectionDB = async () => {
+  config.database = process.env.DB_NAME || 'school_schedule.db';
+  await createConnection(config as ConnectionOptions).then(() => {
+    console.log("sucesso conection servidor");
+  }).catch((e: Error) => {
+    console.log("Erro sucesso conection servidor", e.message);
+  });
+};
